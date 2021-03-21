977a74a3df7c7c35a7a6cb4bd275ce40cb224145
mohammed mustafa <mohammed.alkhddar@gmail.com>
2021-03-21 18:54:07 -0500

    google auth

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-var encrypt = require('mongoose-encryption');
+// var encrypt = require('mongoose-encryption');
+// const md5= require('md5')
+// const bcrypt = require('bcrypt');
+const session = require('express-session')
 
-
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+// const saltRounds = 10;
 
 const app = express();
 
@@ -15,71 +22,198 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.use(session({
+    secret: 'Out little secert',
+    resave: false,
+    saveUninitialized: false
+}));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true ,useUnifiedTopology:true})
+app.use(passport.initialize())
+app.use(passport.session())
 
-const userSchema=new mongoose.Schema({
-email:String,
-password:String
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String
 });
 
 
-var secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret ,encryptedFields: ['password'] });
+// var secret = process.env.SECRET;
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-const User =mongoose.model('User',userSchema)
 
-app.get('/',function (req,res) {
+const User = mongoose.model('User', userSchema)
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+
+// google strategy 
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECERT,
+    callbackURL: "http://localhost:8080/auth/google/secerts",
+    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile)
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+
+            return cb(err, user);
+        });
+    }
+));
+
+
+app.get('/', function (req, res) {
     res.render('home')
 })
 
-app.get('/login',function (req,res) {
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/secerts',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/secerts');
+    });
+app.get('/login', function (req, res) {
     res.render('login')
 })
 
-app.get('/register',function (req,res) {
+app.get('/register', function (req, res) {
+
     res.render('register')
 })
 
 
+app.get('/secerts', function (req, res) {
+// not equal to  $nu null to look all the secerts field in the data base 
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if (err){
+          console.log(err);
+        } else {
+          if (foundUsers) {
+            res.render("secrets", {usersWithSecrets: foundUsers});
+          }
+        }
+      });
+
+});
+
+app.get("/submit", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit", function (req, res) {
+    const submitedSecret = req.body.secret;
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submitedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secerts");
+                });
+            }
+        }
+    });
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+
+    res.redirect('/');
+
+});
 
 //mongoose-encryption will encrypt when it save 
-app.post('/register',function (req,res) {
+app.post('/register', function (req, res) {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err)
+            res.redirect('/register')
+        } else {
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/secerts')
 
-const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-})
-newUser.save(function (err) {
-    if(err){
-        console(err)
-    }else{
-        res.render('secrets');
-    }
-})
-})
+            });
+
+        };
+    });
+    // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    //     // Store hash in your password DB.
+    //     const newUser = new User({
+    //         email: req.body.username,
+    //         password: hash
+    //     })
+    //     newUser.save(function (err) {
+    //         if (err) {
+    //             console(err)
+    //         } else {
+    //             res.render('secrets');
+    //         }
+    //     })
+    // });
+
+});
+
+app.post('/login', function (req, res) {
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect('/secerts')
+
+            })
+        }
+    })
+    // const userName = req.body.username
+    // const password = req.body.password
+    // //mongoose-encryption will decrypt when it find   
+    // User.findOne({ email: userName }).then((foundUser) => {
+    //     if (foundUser) {
+    //         bcrypt.compare(password, foundUser.password, function (err, result) {
+    //             if(result==true){
+    //                 res.render('secrets')
+    //             }
+    //         });
+    //     }
+    // }).catch((err) => {
+    //     console.log('not matched')
+    // })
 
 
+});
 
-
-app.post('/login',function (req,res) {
-    const userName=req.body.username
-    const password=req.body.password
-  //mongoose-encryption will decrypt when it find   
-User.findOne({email: userName}).then((foundUser)=>{
-    if(foundUser.password===password){
-        res.render('secrets')
-    }
-}).catch((err)=>{
-console.log('not matched')
-})
- 
-
-
-
-
-})
-
-app.listen(process.env.HOST,function () {
+app.listen(process.env.HOST, function () {
     console.log('the server is up')
 })
